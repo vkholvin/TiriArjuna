3a436465030f8bb695ec428d76d479ae3284bc62
Yabes123 <immanuelyabes20@gmail.com>
2024-03-19 16:49:37 +0700

    (Create): Routing for Navbar

--- TiriArjuna/src/Home.tsx
+++ TiriArjuna/src/Home.tsx
@@ -1,13 +1,9 @@
-export default function Home (){
-    return(
-        <header className="fixed w-full z-10 flex flex-row justify-between items-center px-5  ">
-        <img src="/assets/trijun.svg" className="flex-row w-56 p-10"></img>
-        <div className="">
-            
+import Navbar from "./component/Navbar";
 
-
-        </div>
-
-        </header>
-    )
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
