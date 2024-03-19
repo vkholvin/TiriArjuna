8eb10e08015844211008ba87e00f5acb4ea681f2
vkholvin <vkholvin@gmail.com>
2024-03-19 13:14:58 +0700

    (create):login page

--- TiriArjuna/tailwind.config.js
+++ TiriArjuna/tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors:{
+      'ungu':'#CECEDC',
+      'putih' : '#ffffff',
+      'abu': '#B2B2B2'
+    },
     extend: {},
   },
   plugins: [],
