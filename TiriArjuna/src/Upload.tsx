672580def70b607cd01c83c18419d1bd9e8cea90
Immanuel Yabes <INT00844@mylab.local>
2024-03-20 16:04:24 +0700

    (Fix) : Upload Page & Navbar

--- TiriArjuna/src/Upload.tsx
+++ TiriArjuna/src/Upload.tsx
@@ -1,15 +1,19 @@
-import Navbar from "./component/Navbar"
+import Navbar from "./component/Navbar";
 
 export default function Upload() {
   return (
-    <div>
+    <div >
       <Navbar />
-      <div className="flex items-center justify-center w-full">
-        <label
-          htmlFor="dropzone-file"
-          className="flex flex-col items-center justify-center w-screen h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-        >
-          <div className="flex flex-col items-center justify-center pt-5 pb-6">
+      <div className="flex flex-col ml-2 pt-10 pl-10 ">
+        <h3 className="text-2xl font-bold">Upload Documents</h3>
+      </div>
+
+        <div className="h-screen flex flex-col items-center  mt-0" >
+          <h3 className="font-bold text-3xl text-center p-8">Upload</h3>
+          <label
+            htmlFor="dropzone-file"
+            className="flex flex-col items-center justify-center w-96 p-24 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+          >
             <svg
               className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
               aria-hidden="true"
@@ -19,9 +23,9 @@ export default function Upload() {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
               />
             </svg>
@@ -32,10 +36,28 @@ export default function Upload() {
             <p className="text-xs text-gray-500 dark:text-gray-400">
               SVG, PNG, JPG or GIF (MAX. 800x400px)
             </p>
+            <input id="dropzone-file" type="file" className="hidden" />
+          </label>
+
+          <div className="pt-2 ">
+          <div className="mx-10 flex border appearance-none peer w-96 rounded-lg ">
+              <input type="text" name="file_name" id="file_name" className="block rounded-lg w-full py-2.5 px-2 w-full text-sm bg-transparent  appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Input File Name" required />
+              <label htmlFor="file_name" className="peer-focus:font-medium absolute dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Input File Name</label>
+            </div>
           </div>
-          <input id="dropzone-file" type="file" className="hidden" />
-        </label>
-      </div>
+
+          <div className="mx-10 pt-10 w-96">
+          <button type="submit" className="w-full flex justify-center p-3 bg-ungu rounded-lg text-xl font-semibold hover:bg-abu">
+           Submit
+          </button>
+          </div>
+
+        </div>
+            
+
+      
+        
+      
     </div>
   );
 }
--- TiriArjuna/src/component/Navbar.tsx
+++ TiriArjuna/src/component/Navbar.tsx
@@ -1,8 +1,8 @@
 export default function Navbar() {
   return (
-    <div>
+    <div className="bg-navbar w-full sticky top-0">
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
-        <div className="width: 100% flex flex-wrap items-center justify-between mx-auto p-5 pr-4">
+        <div className="flex items-center justify-between mx-auto p-5 pr-15">
           <a
             href="https://flowbite.com/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
@@ -30,9 +30,9 @@ export default function Navbar() {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
@@ -42,7 +42,7 @@ export default function Navbar() {
               <li>
                 <a
                   href="/home"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  className="block py-2 px-3 mr-16 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                   aria-current="page"
                 >
                   Home
@@ -51,7 +51,7 @@ export default function Navbar() {
               <li>
                 <a
                   href="/upload"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 mr-16 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Upload
                 </a>
@@ -59,7 +59,7 @@ export default function Navbar() {
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 mr-16 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Logout
                 </a>
