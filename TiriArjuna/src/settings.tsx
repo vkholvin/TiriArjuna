50697ee06f1103e647e3c5c36434de7198fd4299
vkholvin <vkholvin@gmail.com>
2024-03-21 16:14:45 +0700

    (fix): bikin setting

--- TiriArjuna/src/settings.tsx
+++ TiriArjuna/src/settings.tsx
@@ -3,9 +3,18 @@ import Navbar from './component/Navbar'
 
 export default function Settings() {
   return (
-    <div>
+    <div className='bg-ungu w-full h-screen'>
         <Navbar />
-        <h3 className='text-blue-600'>YABES KONTOL</h3>
+        <div className='flex flex-row justify-between p-40'>
+          <div className='flex flex-row  bg-heder p-10'>
+          <h3>tes</h3>
+          </div>
+
+          <div className='flex flex-row bg-heder h-10'>
+          <h3>tes</h3>
+          </div>
+
+        </div>
     </div>
   )
 }
