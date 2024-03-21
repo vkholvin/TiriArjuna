4a06cb9134f53f0b5f7698a48d7602399d65a51d
Immanuel Yabes <INT00844@mylab.local>
2024-03-21 11:07:02 +0700

    (Fix) : Pagination Hover Color

--- TiriArjuna/src/Home.tsx
+++ TiriArjuna/src/Home.tsx
@@ -97,7 +97,7 @@ const Home: React.FC = () => {
                 <button
                   onClick={() => handlePageChange(index + 1)}
                   className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
-                    currentPage === index + 1 ? 'text-blue-600 bg-blue-50' : ''
+                    currentPage === index + 1 ? 'text-blue-600 bg-ungu' : ''
                   }`}
                 >
                   {index + 1}
